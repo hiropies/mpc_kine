779312b6d2bc49573b80d67931eb472681f51540
hiroto <s223087@stn.nagaokaut.ac.jp>
2024-12-05 17:33:57 +0900

    '偏差の重みのみの時に追従できる'

--- SimParameters.m
+++ SimParameters.m
@@ -3,12 +3,16 @@ clear variables
 addpath('func\');
 run("make_cmd.m");
 load("ref23.mat");
-long_cmd = length(pos_ref2);
+
 Ts = 100e-6;
 Ts_plant = 10e-6; % 10  [us]
 Ts_control = 100e-6;  % 100 [us]
 %Ts_MPC = 0.01;    % 
 Ts_MPC = Ts;
+
+long_cmd = length(pos_ref2);
+pos_ref2_tt = array2timetable(pos_ref2,'SampleRate',1/Ts);
+pos_ref3_tt = array2timetable(pos_ref3,'SampleRate',1/Ts);
 
 Kt1 = 4* 0.070504;
 Rg1 = 120;
@@ -34,7 +38,7 @@ w2 = 50.0;
 zeta2 = 1.0;
 Kvp2 = (Jm2*w2*zeta2)/Kt2;
 Kvi2 = (w2*w2*Jm2)/Kt2;
-Kpp2 = 2.0;
+Kpp2 = 5.0;
 
 Kt3 = (0.0631765) * (4);
 Rg3 = 121;
@@ -47,7 +51,7 @@ w3 = 50.0;
 zeta3 = 1.0;
 Kvp3 = (Jm3*w3*zeta3)/Kt3;
 Kvi3 = (w3*w3*Jm3)/Kt3;
-Kpp3 = 2.0;
+Kpp3 = 5.0;
 
 %% State-space representation
 n= 2; %Dimension of state vector: We only have q2,q3
@@ -63,8 +67,8 @@ T = [1,0; 0,1];        %Constraint selection vector. Select which states are to 
 u2c = 50*Rg2;    %Input constraint i.e. q2 limit
 u3c = 50*Rg3;    %Input constraint i.e. q3 limit
 % du1c = 100; %Input rate of change i.e. q1 limit
-du2c = 1000; %Input rate of change i.e. q2 limit
-du3c = 1000; %Input rate of change i.e. q3 limit
+du2c = 100; %Input rate of change i.e. q2 limit
+du3c = 100; %Input rate of change i.e. q3 limit
 u_MAX = [u2c;
          u3c];
 u_MIN = [-u2c;
@@ -75,8 +79,8 @@ du_MIN = [-du2c;
           -du3c;];
 
 %Weights (Reference tracking, input penalization and input rate penalization)
-w_E = 1.0;
-w_DU = 0.0;
+w_E = 100;
+w_DU = 0.1;
 w_A = 0.0;
 
 %P: Prediction horizon / L: Control horizon
@@ -89,7 +93,7 @@ J0 = jacobi23(0,0);
 [Q,q1,q2,D,DU_MAX,DU_MIN,U_MAX,U_MIN, M, LT, W_E, W_DU, W_A, Sv, Sa, n, ng] = LinearMPCPrecomputer(J0,T,P,L,w_E,w_DU,w_A,u_MAX,u_MIN,du_MAX,du_MIN,Ts_MPC);
 
 tic;
-%out = sim('OneMassMPC.slx');
+% out = sim('KineMPC.slx');
 toc;
 
 %{
@@ -138,3 +142,4 @@ grid on;
 save("param.mat");
 params = load("param.mat");
 disp('Finished');
+%}
--- make_cmd.m
+++ make_cmd.m
@@ -32,17 +32,20 @@ end
 LH = length(motor_rad);
 motor_rad = reshape(motor_rad,[LH(1),6]);
 joint_rad = reshape(joint_rad,[LH(1),6]);
+cmd_rad = joint_rad;
 
 time_ramp = 1.0;
-pos_ref_go = zeros((time_ramp)/Ts+1,6);
+pos_ref_go = zeros((time_ramp+3.0)/Ts+1,6);
 pos_ref_end = zeros((1.0)/Ts,6);
 for i = 1:6
-    pos_go = trapezoidal(joint_rad(1,i),time_ramp,Ts);
-    pos_ref_go(:,i) = pos_go;
-    pos_ref_end(:,i) = joint_rad(LH(1),i);
+    pos_go = trapezoidal(cmd_rad(1,i),time_ramp,Ts);
+    ll = length(pos_go);
+    pos_ref_go(:,i) = pos_go(ll);
+    pos_ref_go(1:ll,i) = pos_go;
+    pos_ref_end(:,i) = cmd_rad(LH(1),i);
 end
 
-pos_go_traj = vertcat(pos_ref_go,joint_rad);
+pos_go_traj = vertcat(pos_ref_go,cmd_rad);
 pos_go_traj = vertcat(pos_go_traj,pos_ref_end);
 
 pos_ref = array2timetable(pos_go_traj,'SampleRate',1/Ts);
@@ -50,15 +53,19 @@ pos_ref2 = array2timetable(pos_go_traj(:,2),'SampleRate',1/Ts);
 pos_ref3 = array2timetable(pos_go_traj(:,3),'SampleRate',1/Ts);
 qref2_M_deg = rad2deg_array(pos_ref2.Var1);
 qref3_M_deg = rad2deg_array(pos_ref3.Var1);
-pos_ref2 = qref2_M_deg;
-pos_ref3 = qref2_M_deg;
+qref2_M_rad = deg2rad_array(qref2_M_deg);
+qref3_M_rad = deg2rad_array(qref3_M_deg);
+pos_ref2 = qref2_M_rad;
+pos_ref3 = qref3_M_rad;
 %pos_ref2 = array2timetable(qref2_M_deg,'SampleRate',1/Ts,'VariableNames',{'Var1'});
 %pos_ref3 = array2timetable(qref3_M_deg,'SampleRate',1/Ts,'VariableNames',{'Var1'});
 %save('reference.mat',"pos_ref");
 save('ref23.mat',"pos_ref2","pos_ref3");
 % save('D:\user\Desktop\abe\matlab\sim\20240918_軌跡シミュ_四角\reference.mat',"pos_ref");
 
-%{
+x_ = goal_shift.pos(2,:);
+y_ = goal_shift.pos(3,:);
+z_ = goal_shift.pos(4,:);
 %% figure作成
 set(0,'DefaultTextInterpreter','latex')
 set(0,'DefaultLegendInterpreter','latex')
@@ -107,29 +114,28 @@ grid on;
 
 %% 3枚目
 nexttile
-h31 = plot(time,y_);
+h31 = plot(pos_ref.Time,pos_ref2);
 set(h31, 'linewidth',linewidth);
 hold on;
 % 表示範囲指定
-xrange = [0 2502];
-yrange = [-0.2 1.0];
+%xrange = [0 2502];
+%yrange = [-0.2 1.0];
 %set(gca, 'Xlim',xrange); 
 %set(gca, 'Ylim',yrange);
 xlabel({'time[s]'},'FontSize',font, 'color','k');
-ylabel({'Y[m]'},'FontSize',font, 'color','k');
+ylabel({'Axis2_M[rad]'},'FontSize',font, 'color','k');
 grid on;
 
 %% 2枚目
 nexttile
-h21 = plot(time,x_);
+h21 = plot(pos_ref.Time,pos_ref3);
 set(h21, 'linewidth',linewidth);
 hold on;
 % 表示範囲指定
-xrange = [0 2502];
-yrange = [-0.2 1.0];
+%xrange = [0 2502];
+%yrange = [-0.2 1.0];
 %set(gca, 'Xlim',xrange); 
 %set(gca, 'Ylim',yrange);
 xlabel({'time[s]'},'FontSize',font, 'color','k');
-ylabel({'X[m]'},'FontSize',font, 'color','k');
+ylabel({'Axis3_M[rad]'},'FontSize',font, 'color','k');
 grid on;
-%}
